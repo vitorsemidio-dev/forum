8815677e03956b72a2ba366cedfc217bf18c4695
Vitor Emidio <vitorsemidio.96@gmail.com>
2023-05-05 20:45:43 -0300

    feat: answer question

---
+++ src/domain/use-cases/answer-question.spec.ts
@@ -0,0 +1,17 @@
+import { test } from 'vitest';
+import { AnswerQuestionUseCase } from './answer-question';
+
+test('create an answer', async ({ expect }) => {
+  const answerQuestionUseCase = new AnswerQuestionUseCase();
+
+  const { answer } = await answerQuestionUseCase.execute({
+    instructorId: 'any_instructor_id',
+    questionId: 'any_question_id',
+    content: 'any_content',
+  });
+
+  expect(answer).toEqual({
+    id: expect.any(String),
+    content: 'any_content',
+  });
+});
---
+++ src/domain/use-cases/answer-question.ts
@@ -0,0 +1,23 @@
+import { Answer } from '../entities/answer';
+
+interface AnswerQuestionUseCaseRequest {
+  instructorId: string;
+  questionId: string;
+  content: string;
+}
+
+interface AnswerQuestionUseCaseResponse {
+  answer: Answer;
+}
+
+export class AnswerQuestionUseCase {
+  async execute({
+    instructorId,
+    questionId,
+    content,
+  }: AnswerQuestionUseCaseRequest): Promise<AnswerQuestionUseCaseResponse> {
+    const answer = new Answer(content);
+
+    return { answer };
+  }
+}
