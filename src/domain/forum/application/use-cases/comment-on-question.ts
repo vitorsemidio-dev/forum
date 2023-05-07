49b871ef41113c169645d8f358a0b965d9e3e221
Vitor Emídio <vitorsemidio.96@gmail.com>
2023-05-07 14:52:59 -0300

    feat: comment on question

---
+++ src/domain/forum/application/use-cases/comment-on-question.ts
@@ -0,0 +1,45 @@
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import { QuestionCommentsRepository } from '@/domain/forum/application/repositories/question-comments-repository'
+import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
+import { QuestionComment } from '@/domain/forum/enterprise/entities/question-comment'
+
+interface CommentOnQuestionUseCaseRequest {
+  authorId: string
+  questionId: string
+  content: string
+}
+
+interface CommentOnQuestionUseCaseResponse {
+  questionComment: QuestionComment
+}
+
+export class CommentOnQuestionUseCase {
+  constructor(
+    private questionsRepository: QuestionsRepository,
+    private questionCommentsRepository: QuestionCommentsRepository,
+  ) {}
+
+  async execute({
+    authorId,
+    questionId,
+    content,
+  }: CommentOnQuestionUseCaseRequest): Promise<CommentOnQuestionUseCaseResponse> {
+    const question = await this.questionsRepository.findById(questionId)
+
+    if (!question) {
+      throw new Error('Question not found.')
+    }
+
+    const questionComment = QuestionComment.create({
+      authorId: new UniqueEntityId(authorId),
+      questionId: new UniqueEntityId(questionId),
+      content,
+    })
+
+    await this.questionCommentsRepository.create(questionComment)
+
+    return {
+      questionComment,
+    }
+  }
+}
