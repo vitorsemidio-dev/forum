b083a11dc2a81b7c21900ba3a8167630f6250360
Vitor Emídio <vitorsemidio.96@gmail.com>
2023-05-07 14:52:43 -0300

    feat: comment on answer

---
+++ src/domain/forum/application/use-cases/comment-on-answer.ts
@@ -0,0 +1,45 @@
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import { AnswerCommentsRepository } from '@/domain/forum/application/repositories/answer-comments-repository'
+import { AnswersRepository } from '@/domain/forum/application/repositories/answers-repository'
+import { AnswerComment } from '@/domain/forum/enterprise/entities/answer-comment'
+
+interface CommentOnAnswerUseCaseRequest {
+  authorId: string
+  answerId: string
+  content: string
+}
+
+interface CommentOnAnswerUseCaseResponse {
+  answerComment: AnswerComment
+}
+
+export class CommentOnAnswerUseCase {
+  constructor(
+    private answersRepository: AnswersRepository,
+    private answerCommentsRepository: AnswerCommentsRepository,
+  ) {}
+
+  async execute({
+    authorId,
+    answerId,
+    content,
+  }: CommentOnAnswerUseCaseRequest): Promise<CommentOnAnswerUseCaseResponse> {
+    const answer = await this.answersRepository.findById(answerId)
+
+    if (!answer) {
+      throw new Error('Answer not found.')
+    }
+
+    const answerComment = AnswerComment.create({
+      authorId: new UniqueEntityId(authorId),
+      answerId: new UniqueEntityId(answerId),
+      content,
+    })
+
+    await this.answerCommentsRepository.create(answerComment)
+
+    return {
+      answerComment,
+    }
+  }
+}
