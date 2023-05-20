fec4691fcb32a0f2e5196f473a1fbec1c2f98ac4
Vitor Emídio <vitorsemidio.96@gmail.com>
2023-05-20 13:53:54 -0300

    feat: criação tipagem either e auxiliares

---
+++ src/core/either.ts
@@ -0,0 +1,36 @@
+export class Left<L, R> {
+  readonly value: L
+
+  constructor(value: L) {
+    this.value = value
+  }
+
+  isLeft(): this is Left<L, R> {
+    return true
+  }
+
+  isRight(): this is Right<L, R> {
+    return false
+  }
+}
+
+export class Right<L, R> {
+  readonly value: R
+
+  constructor(value: R) {
+    this.value = value
+  }
+
+  isLeft(): this is Left<L, R> {
+    return false
+  }
+
+  isRight(): this is Right<L, R> {
+    return true
+  }
+}
+
+export type Either<L, R> = Left<L, R> | Right<L, R>
+
+export const left = <L, R>(value: L): Either<L, R> => new Left(value)
+export const right = <L, R>(value: R): Either<L, R> => new Right(value)
