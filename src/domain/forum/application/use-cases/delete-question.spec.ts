d9ec187fd5c81e89e9bfd18a49d5162d050f2d4e
Vitor Emidio <vitorsemidio.96@gmail.com>
2023-06-17 20:11:10 -0300

    test: delete question with attachments

--- src/domain/forum/application/use-cases/delete-question.spec.ts
+++ src/domain/forum/application/use-cases/delete-question.spec.ts
@@ -2,6 +2,7 @@ import { UniqueEntityId } from '@/core/entities/unique-entity-id'
 import { DeleteQuestionUseCase } from '@/domain/forum/application/use-cases/delete-question'
 import { NotAllowedError } from '@/domain/forum/application/use-cases/errors/not-allowed.error'
 import { makeQuestion } from 'test/factories/make-question'
+import { makeQuestionAttachment } from 'test/factories/make-question-attachment'
 import { InMemoryQuestionAttachmentsRepository } from 'test/repositories/in-memory-question-attachments-repository'
 import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
 
@@ -37,6 +38,36 @@ describe('DeleteQuestionUseCase', () => {
 
     expect(result.isRight()).toBe(true)
     expect(inMemoryQuestionsRepository.items.length).toBe(1)
+  })
+
+  it('should delete a question with attachments', async () => {
+    const newQuestion = makeQuestion(
+      {
+        authorId: new UniqueEntityId('author-id-1'),
+      },
+      new UniqueEntityId('question-id-1'),
+    )
+    await inMemoryQuestionsRepository.create(newQuestion)
+
+    inMemoryQuestionAttachmentsRepository.items.push(
+      makeQuestionAttachment({
+        attachmentId: new UniqueEntityId('attachment-id-1'),
+        questionId: newQuestion.id,
+      }),
+      makeQuestionAttachment({
+        attachmentId: new UniqueEntityId('attachment-id-2'),
+        questionId: newQuestion.id,
+      }),
+    )
+
+    const result = await sut.execute({
+      questionId: 'question-id-1',
+      authorId: 'author-id-1',
+    })
+
+    expect(result.isRight()).toBe(true)
+    expect(inMemoryQuestionsRepository.items.length).toBe(0)
+    expect(inMemoryQuestionAttachmentsRepository.items.length).toBe(0)
   })
 
   it('should not delete a question from another user', async () => {
