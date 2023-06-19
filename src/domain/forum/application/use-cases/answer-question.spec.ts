03d52f928faf1b29dd031e6d163d2c1a9c5066a8
Vitor Emídio <vitorsemidio.96@gmail.com>
2023-06-18 22:24:34 -0300

    test: add test for create answer question with attachments

--- src/domain/forum/application/use-cases/answer-question.spec.ts
+++ src/domain/forum/application/use-cases/answer-question.spec.ts
@@ -10,7 +10,7 @@ describe('AnswerQuestionUseCase', () => {
     sut = new AnswerQuestionUseCase(inMemoryAnswersRepository)
   })
 
-  it('create an answer', async ({ expect }) => {
+  it('create an answer without attachments', async ({ expect }) => {
     const result = await sut.execute({
       attachmentIds: [],
       instructorId: 'any_instructor_id',
@@ -23,6 +23,25 @@ describe('AnswerQuestionUseCase', () => {
     expect(result.value?.answer.content).toBe('any_content')
     expect(inMemoryAnswersRepository.items[0].id.toValue()).toBe(
       result.value?.answer.id.toValue(),
+    )
+  })
+
+  it('should create an answer with attachments', async () => {
+    const result = await sut.execute({
+      attachmentIds: ['any_attachment_id'],
+      instructorId: 'any_instructor_id',
+      questionId: 'any_question_id',
+      content: 'any_content',
+    })
+
+    const answer = inMemoryAnswersRepository.items[0]
+    const answerAttachments = answer.attachments.getItems()
+
+    expect(result.isRight()).toBe(true)
+    expect(answer).toBe(result.value?.answer)
+    expect(answerAttachments.length).toBe(1)
+    expect(answerAttachments[0].attachmentId.toString()).toBe(
+      'any_attachment_id',
     )
   })
 })
