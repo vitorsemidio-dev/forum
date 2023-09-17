e191c13575648f05e326ebc87d122d7db36a80fa
Vitor Emídio <vitorsemidio.96@gmail.com>
2023-09-17 16:47:06 -0300

    feat: Criação env module com variáveis ambientes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common'
 import { CryptographyModule } from './infra/cryptography/cryptography.module'
 import { PrismaService } from './infra/database/prisma/prisma.service'
+import { EnvModule } from './infra/env/env.module'
 import { CreateAccountController } from './infra/http/controller/create-account.controller'
 
 @Module({
-  imports: [CryptographyModule],
+  imports: [EnvModule, CryptographyModule],
   controllers: [CreateAccountController],
   providers: [PrismaService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { EnvService } from './infra/env/env.service'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule)
+
+  const configService = app.get(EnvService)
+  const port = configService.get('PORT')
+
+  await app.listen(port)
 }
-bootstrap();
+bootstrap()
