0b9cf7b34346579d8c6bd00a78150c675c66a9dd
Vitor Emidio <vitorsemidio.96@gmail.com>
2023-09-21 02:16:18 -0300

    test: Criação de conta

---
+++ src/infra/http/controller/create-account.controller.e2e-spec.ts
@@ -0,0 +1,44 @@
+import { AppModule } from '@/app.module'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { INestApplication } from '@nestjs/common'
+import { Test } from '@nestjs/testing'
+import request from 'supertest'
+import { UserFactory, makeUser } from 'test/factories/make-user'
+
+describe('Create Account (E2E)', () => {
+  let app: INestApplication
+  let prisma: PrismaService
+  let userFactory: UserFactory
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+      providers: [PrismaService, UserFactory],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+    prisma = moduleRef.get(PrismaService)
+    userFactory = moduleRef.get(UserFactory)
+
+    await app.init()
+  })
+
+  test('[POST] /accounts', async () => {
+    const user = makeUser()
+    const response = await request(app.getHttpServer())
+      .post('/accounts')
+      .send(user)
+
+    expect(response.statusCode).toBe(201)
+
+    const userOnDatabase = await prisma.user.findUnique({
+      where: {
+        email: user.email,
+      },
+    })
+
+    expect(userOnDatabase).toBeTruthy()
+    expect(userOnDatabase?.password).toBeDefined()
+    expect(userOnDatabase?.password).not.toBe(user.password)
+  })
+})
--- src/infra/http/controller/test-e2e.e2e-spec.ts
+++
@@ -1,5 +0,0 @@
-describe('should test', () => {
-  test('should test', () => {
-    expect(1).toBe(1)
-  })
-})
