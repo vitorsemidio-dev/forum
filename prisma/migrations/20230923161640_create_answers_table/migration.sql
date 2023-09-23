4f6e38ee8d29debc3d6760ccb98975630daf37e8
Vitor Emídio <vitorsemidio.96@gmail.com>
2023-09-23 13:20:24 -0300

    feat: Criação tabela answers no banco de dados

---
+++ prisma/migrations/20230923161640_create_answers_table/migration.sql
@@ -0,0 +1,38 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[best_answer_id]` on the table `questions` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- AlterTable
+ALTER TABLE "attachments" ADD COLUMN     "answerId" TEXT;
+
+-- AlterTable
+ALTER TABLE "questions" ADD COLUMN     "best_answer_id" TEXT;
+
+-- CreateTable
+CREATE TABLE "answers" (
+    "id" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3),
+    "author_id" TEXT NOT NULL,
+    "question_id" TEXT NOT NULL,
+
+    CONSTRAINT "answers_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "questions_best_answer_id_key" ON "questions"("best_answer_id");
+
+-- AddForeignKey
+ALTER TABLE "questions" ADD CONSTRAINT "questions_best_answer_id_fkey" FOREIGN KEY ("best_answer_id") REFERENCES "answers"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "attachments" ADD CONSTRAINT "attachments_answerId_fkey" FOREIGN KEY ("answerId") REFERENCES "answers"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "answers" ADD CONSTRAINT "answers_author_id_fkey" FOREIGN KEY ("author_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "answers" ADD CONSTRAINT "answers_question_id_fkey" FOREIGN KEY ("question_id") REFERENCES "questions"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
