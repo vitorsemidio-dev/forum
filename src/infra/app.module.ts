b0ff829cc7a0a33ebe41c6d8256a001ee6747c7d
Vitor Emídio <vitorsemidio.96@gmail.com>
2023-09-30 14:44:08 -0300

    feat: Captura e tratamento exceptions na camada de infraestrutura

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -1,8 +1,16 @@
 import { EnvModule } from '@/infra/env/env.module'
+import { AllExceptionFilter } from '@/infra/filters/all-exception.filter'
 import { HttpModule } from '@/infra/http/http.module'
 import { Module } from '@nestjs/common'
+import { APP_FILTER } from '@nestjs/core'
 
 @Module({
   imports: [EnvModule, HttpModule],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: AllExceptionFilter,
+    },
+  ],
 })
 export class AppModule {}
---
+++ src/infra/filters/all-exception.filter.ts
@@ -0,0 +1,107 @@
+import { NotAllowedError } from '@/core/errors/not-allowed.error'
+import { ResourceNotFoundError } from '@/core/errors/resource-not-found.error'
+import { InvalidAttachmentTypeError } from '@/domain/forum/application/use-cases/errors/invalid-attachment-type.error'
+import {
+  ArgumentsHost,
+  BadRequestException,
+  Catch,
+  ExceptionFilter,
+  ForbiddenException,
+  HttpException,
+  HttpStatus,
+  Logger,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common'
+import { Request, Response } from 'express'
+
+export class BaseResponseDto {
+  statusCode!: number
+  message!: string
+}
+
+export class ExceptionResponseDto extends BaseResponseDto {
+  timestamp!: string
+  path!: string
+  method!: string
+}
+
+@Catch()
+export class AllExceptionFilter implements ExceptionFilter {
+  catch(exception: any, host: ArgumentsHost) {
+    const ctx = host.switchToHttp()
+    const response = ctx.getResponse<Response>()
+    const request = ctx.getRequest<Request>()
+    let message: string
+    let status: number
+    let logInConsole = true
+
+    switch (exception.constructor) {
+      case ResourceNotFoundError:
+        status = HttpStatus.NOT_FOUND
+        message = (exception as ResourceNotFoundError).message
+        break
+      case NotAllowedError:
+        status = HttpStatus.FORBIDDEN
+        message = (exception as NotAllowedError).message
+        break
+      case InvalidAttachmentTypeError:
+        status = HttpStatus.BAD_REQUEST
+        message = (exception as InvalidAttachmentTypeError).message
+        break
+      case HttpException:
+        status = (exception as HttpException).getStatus()
+        message = (exception as HttpException).message
+        break
+      case BadRequestException:
+        status = HttpStatus.BAD_REQUEST
+        message = (exception as BadRequestException).message
+        break
+      case NotFoundException:
+        status = HttpStatus.NOT_FOUND
+        message = (exception as NotFoundException).message
+        logInConsole = message.includes('Cannot') ? false : logInConsole
+        break
+      case UnauthorizedException:
+        status = HttpStatus.UNAUTHORIZED
+        message = (exception as UnauthorizedException).message
+        break
+      case ForbiddenException:
+        status = HttpStatus.FORBIDDEN
+        message = (exception as ForbiddenException).message
+        break
+      default:
+        status = (exception as any).status ?? HttpStatus.INTERNAL_SERVER_ERROR
+        message = (exception as any).message ?? 'Internal server error'
+        break
+    }
+
+    if (logInConsole)
+      Logger.error(
+        message,
+        (exception as any).stack,
+        `${request.method} ${request.url}`,
+      )
+
+    response
+      .status(status)
+      .json(this.GlobalResponseError(status, message, request))
+  }
+  private GlobalResponseError: (
+    statusCode: number,
+    message: string,
+    request: Request,
+  ) => ExceptionResponseDto = (
+    statusCode: number,
+    message: string,
+    request: Request,
+  ): ExceptionResponseDto => {
+    return {
+      statusCode: statusCode,
+      message,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+      method: request.method,
+    }
+  }
+}
