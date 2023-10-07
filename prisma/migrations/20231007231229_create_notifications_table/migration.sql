6765bc626acb842f51d4d512b521fa930fff385d
Vitor Emídio <vitorsemidio.96@gmail.com>
2023-10-07 20:16:56 -0300

    feat: Criação tabelas de notificações

---
+++ prisma/migrations/20231007231229_create_notifications_table/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "notifications" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "read_at" TIMESTAMP(3),
+    "recipient_id" TEXT NOT NULL,
+
+    CONSTRAINT "notifications_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "notifications" ADD CONSTRAINT "notifications_recipient_id_fkey" FOREIGN KEY ("recipient_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
